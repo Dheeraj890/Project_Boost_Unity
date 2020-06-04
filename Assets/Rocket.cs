66ac5ef51325241a34a76f1062fc932986c60c57
Dheeraj Kandpal <dheeraj.kandpal89@gmail.com>
2020-06-05 00:25:58 +0530

    Rocket creation
    Rocket created with functionalities

---
+++ Assets/Rocket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+
+            rigidbody.AddRelativeForce(Vector3.up);
+            print("thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            transform.Rotate(Vector3.forward);
+            print("rotating left");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.Rotate(-Vector3.forward);
+
+
+            print("rotating right");
+        }
+    }
+}
