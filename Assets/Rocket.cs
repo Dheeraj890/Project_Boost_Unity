4b7e9c0ba2e7a354f525078af4900418e3f608ca
Dheeraj Kandpal <dheeraj.kandpal89@gmail.com>
2020-06-05 16:03:40 +0530

    Audio Source added in Rocket module

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -8,10 +8,14 @@ public class Rocket : MonoBehaviour
 
 
     Rigidbody rigidbody;
+
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,7 +32,19 @@ public class Rocket : MonoBehaviour
 
 
             rigidbody.AddRelativeForce(Vector3.up);
+
+            if (!audioSource.isPlaying)
+            {
+
+                audioSource.Play();
+            }
+            
             print("thrusting");
+        }
+        else
+        {
+
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
